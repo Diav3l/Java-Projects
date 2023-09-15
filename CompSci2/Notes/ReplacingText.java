faa685038562466d84f9ab41e895d525c539ff42
Diav3l <116712498+Diav3l@users.noreply.github.com>
2023-09-14 18:15:42 -0600

    misc maven actions

--- CompSci2/Notes/ReplacingText.java
+++
@@ -1,6 +0,0 @@
-package CompSci2.Notes;
-public class ReplacingText {
-    public static void main(String[] args){
-        
-    }
-}
--- CompSci2/Notes/week2.java
+++
@@ -1,8 +0,0 @@
-package CompSci2.Notes;
-class week2{
-    public static void main(String[] args){
-        int[] nums = new int[0];
-
-        System.out.println(nums.length);
-    }
-}
